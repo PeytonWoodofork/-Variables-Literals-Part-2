72f27a04d0d519777fccb62c9d6914983540e497
PeytonWoodofork <peyton.woodofork@hgs.hiddengeniusproject.org>
2022-01-14 02:27:28 +0000

    Created Variables and Literals

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+  String name = "Peyton" ;
+  int age = 15;
+  String dreamjob ="Physical Assistant";
+
+System.out.println("My name is " + name + ". I am " + age + " years old" + ". When I grow up I want to be a " + dreamjob);
+  }
+}
